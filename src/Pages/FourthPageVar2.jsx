fe98f8cabef0526c9491e9e34bd9f36eb8800b2d
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-10-10 23:41:21 +0300

    upd

--- src/Pages/FourthPageVar2.jsx
+++ src/Pages/FourthPageVar2.jsx
@@ -10,6 +10,7 @@ import ReturnSection from "../Components/ReturnSection/ReturnSection";
 import heroImage from "../Images/heroImage3.webp";
 import heroImageMobile from "../Images/heroImage3Mobile.webp";
 import Form from "../Components/Form/Form";
+import { useEffect } from "react";
 
 const FourthPageVar2 = (props) => {
   localStorage.setItem("pageNumber", 4);
@@ -25,6 +26,44 @@ const FourthPageVar2 = (props) => {
     });
   });
 
+  useEffect(() => {
+    // Вставляємо код GTM в <head> при завантаженні компонента
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TR5VNZ4');
+    `;
+    document.head.appendChild(script);
+
+    // Вставляємо код GTM (noscript) після відкриваючого <body> тегу
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR5VNZ4"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `;
+    document.body.insertBefore(noscript, document.body.firstChild);
+
+    // Функція очистки після видалення компонента
+    return () => {
+      const script = document.querySelector('script[src*="gtm.js"]');
+      const noscript = document.querySelector('noscript iframe[src*="ns.html"]');
+      if (script) {
+        script.remove();
+      }
+      if (noscript) {
+        noscript.remove();
+      }
+    };
+  }, []);
+
+
   return (
     <>
       <HeroSection
@@ -38,7 +77,7 @@ const FourthPageVar2 = (props) => {
       />
       <RecipeSection lang={props.langData} setShowForm={setShowForm} />
       <WaysSection
-        text="The Power of Morgi's Monetization: 4 Fantastic Options"
+        text="The Power of Morgi's Morgis' Monetization"
         lang={props.langData}
         image="waysBg4"
         setShowForm={setShowForm}
