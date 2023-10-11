046dcb7003e723d3647ea4cec4979c69c722c53c
Mykola Perebenesiuk <exodusdevelop@gmail.com>
2023-10-11 15:56:04 +0300

    fixed gtm

--- src/Pages/FourthPageVar2.jsx
+++ src/Pages/FourthPageVar2.jsx
@@ -26,43 +26,6 @@ const FourthPageVar2 = (props) => {
     });
   });
 
-  useEffect(() => {
-    // Вставляємо код GTM в <head> при завантаженні компонента
-    const script = document.createElement('script');
-    script.innerHTML = `
-      (function(w,d,s,l,i){
-        w[l]=w[l]||[];
-        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-        var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;
-        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-TR5VNZ4');
-    `;
-    document.head.appendChild(script);
-
-    // Вставляємо код GTM (noscript) після відкриваючого <body> тегу
-    const noscript = document.createElement('noscript');
-    noscript.innerHTML = `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR5VNZ4"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `;
-    document.body.insertBefore(noscript, document.body.firstChild);
-
-    // Функція очистки після видалення компонента
-    return () => {
-      const script = document.querySelector('script[src*="gtm.js"]');
-      const noscript = document.querySelector('noscript iframe[src*="ns.html"]');
-      if (script) {
-        script.remove();
-      }
-      if (noscript) {
-        noscript.remove();
-      }
-    };
-  }, []);
-
 
   return (
     <>
